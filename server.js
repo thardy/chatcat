32b52cbf35941ac903b25ae4b01a4e0d865079b6
Tim Hardy <tim.hardy@omegacreations.com>
2017-03-17 09:51:50 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express');
+const app = express();
+app.set('port', process.env.PORT || 3001);
+
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello Express!</h1>');
+});
+
+app.get('/dashboard', (req, res, next) => {
+    res.send('<h1>This is the dashboard page!</h1>');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('ChatCAT Running on Port: ', app.get('port'));
+});
+
